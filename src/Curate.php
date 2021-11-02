4b519245504214fcbc9632109a8b1791a2e89f23
Laurens Bultynck <laurens@3daimtrainer.com>
2021-11-02 18:14:53 +0100

    Refactor removal of unused references as curator

--- src/Curate.php
+++ src/Curate.php
@@ -9,9 +9,7 @@ use Beblife\SpecCurator\Curators\Paths;
 use Beblife\SpecCurator\Curators\Security;
 use Beblife\SpecCurator\Curators\Servers;
 use Beblife\SpecCurator\Curators\WithoutTags;
-use cebe\openapi\spec\Reference;
-use RecursiveArrayIterator;
-use RecursiveIteratorIterator;
+use Beblife\SpecCurator\Curators\WithoutUnusedReferences;
 
 final class Curate
 {
@@ -70,71 +68,10 @@ final class Curate
 
     public function toCuratedSpec(): Spec
     {
-        $this->curated = array_reduce($this->curators, function (Spec $spec, Curator $curator) {
+        $this->withCurator(new WithoutUnusedReferences());
+
+        return array_reduce($this->curators, function (Spec $spec, Curator $curator) {
             return $curator->curate($spec);
         }, $this->curated);
-
-        $this->removeUnusedReferences();
-
-        return $this->curated;
-    }
-
-    private function removeUnusedReferences(): void
-    {
-        $specProperties =  new RecursiveIteratorIterator(new RecursiveArrayIterator($this->curated->toArray()));
-
-        $usedReferences = [];
-
-        foreach ($specProperties as $key => $value) {
-            if ($key === '$ref') {
-                array_push($usedReferences, new Reference([
-                    '$ref' => $value,
-                ]));
-            }
-
-            if (is_string($value) && substr($value, 0, 2) === '#/' && strpos(serialize($this->curated->paths->getPaths()), '"discriminator"')) {
-                array_push($usedReferences, new Reference([
-                    '$ref' => $value,
-                ]));
-            }
-        }
-
-        $usedReferencesWithSection = array_map(function (Reference $ref) {
-            [$_, $section, $key] = $ref->getJsonReference()->getJsonPointer()->getPath();
-
-            return [
-                'section' => $section,
-                'key' => $key,
-            ];
-        }, $usedReferences);
-
-        $groupedReferences = array_reduce($usedReferencesWithSection, function ($usedReferences, $usedReference) {
-            $usedReferences[$usedReference['section']][] = $usedReference['key'];
-
-            return $usedReferences;
-        }, [
-            'schemas' => [],
-            'responses' => [],
-        ]);
-
-        if (isset($this->curated->components->responses) && count($this->curated->components->responses)) {
-            $this->curated->components->responses = array_filter($this->curated->components->responses, function ($key) use ($groupedReferences) {
-                return in_array($key, $groupedReferences['responses'], false);
-            }, ARRAY_FILTER_USE_KEY);
-
-            if (empty($this->curated->components->responses)) {
-                unset($this->curated->components->responses);
-            }
-        }
-
-        if (isset($this->curated->components->schemas) && count($this->curated->components->schemas)) {
-            $this->curated->components->schemas = array_filter($this->curated->components->schemas, function ($key) use ($groupedReferences) {
-                return in_array($key, $groupedReferences['schemas'], true);
-            }, ARRAY_FILTER_USE_KEY);
-
-            if (empty($this->curated->components->schemas)) {
-                unset($this->curated->components->schemas);
-            }
-        }
     }
 }
---
+++ src/Curators/WithoutUnusedReferences.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Beblife\SpecCurator\Curators;
+
+use Beblife\SpecCurator\Spec;
+use cebe\openapi\spec\Reference;
+use RecursiveArrayIterator;
+use RecursiveIteratorIterator;
+
+final class WithoutUnusedReferences implements Curator
+{
+    private array $usedReferences = [];
+
+    public function curate(Spec $spec): Spec
+    {
+        $this->determineUsedReferences($spec);
+
+        $spec = $this->removeUnusedFromSection($spec, 'responses');
+        $spec = $this->removeUnusedFromSection($spec, 'schemas');
+
+        return $spec;
+    }
+
+    private function determineUsedReferences(Spec $spec): void
+    {
+        $specProperties =  new RecursiveIteratorIterator(new RecursiveArrayIterator($spec->toArray()));
+
+        foreach ($specProperties as $key => $value) {
+            if ($key === '$ref') {
+                array_push($this->usedReferences, new Reference([
+                    '$ref' => $value,
+                ]));
+            }
+
+            if (is_string($value) && substr($value, 0, 2) === '#/' && strpos(serialize($spec->paths->getPaths()), '"discriminator"')) {
+                array_push($this->usedReferences, new Reference([
+                    '$ref' => $value,
+                ]));
+            }
+        }
+
+        $this->usedReferences = array_map(function (Reference $ref) {
+            [$_, $section, $key] = $ref->getJsonReference()->getJsonPointer()->getPath();
+
+            return [
+                'section' => $section,
+                'key' => $key,
+            ];
+        }, $this->usedReferences);
+
+        $this->usedReferences = array_reduce($this->usedReferences, function ($usedReferences, $usedReference) {
+            $usedReferences[$usedReference['section']][] = $usedReference['key'];
+
+            return $usedReferences;
+        }, [
+            'schemas' => [],
+            'responses' => [],
+        ]);
+    }
+
+    private function removeUnusedFromSection(Spec $spec, string $section): Spec
+    {
+        if (isset($spec->components->{$section}) && count($spec->components->{$section})) {
+            $spec->components->{$section} = array_filter($spec->components->{$section}, function ($key) use ($section) {
+                return in_array($key, $this->usedReferences[$section], false);
+            }, ARRAY_FILTER_USE_KEY);
+
+            if (empty($spec->components->{$section})) {
+                unset($spec->components->{$section});
+            }
+        }
+
+        return $spec;
+    }
+}
